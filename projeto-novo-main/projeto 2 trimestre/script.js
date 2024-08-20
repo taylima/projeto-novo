7fef71056bbf5c05c71829591fa7f341ea93ad5b
taylima <limatainara937@gmail.com>
2024-08-20 08:50:59 -0300

    Add files via upload

---
+++ projeto-novo-main/projeto 2 trimestre/script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia"
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "afirmacao"
+            }
+
+        ]
+},
+{
+    enunciado: "com a descoberta da tecnologia, chamada inteligencia artificial (IA)"
+}
